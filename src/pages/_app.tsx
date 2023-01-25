b86c32dbc75c0f159a856aa7a784dbc9e22c5b08
adachi-shu <backnumber0620@icloud.com>
2023-01-25 16:08:53 +0900

    add next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Zen_Kaku_Gothic_Antique } from "@next/font/google";
+
+const ZenKakuGothicAntique_normal = Zen_Kaku_Gothic_Antique({
+  weight: "400",
+  subsets: ["cyrillic"],
+});
+const ZenKakuGothicAntique_bold = Zen_Kaku_Gothic_Antique({
+  weight: "700",
+  subsets: ["cyrillic"],
+});
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+         font-family: ${ZenKakuGothicAntique_normal.style.fontFamily},
+           ${ZenKakuGothicAntique_bold};
+       }
+     `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
