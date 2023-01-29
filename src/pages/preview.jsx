4bcd451519f2b74878cc2613ed5f7c063c01e6b6
adachi-shu <backnumber0620@icloud.com>
2023-01-29 22:54:56 +0900

    edit preview.jsx

--- src/pages/preview.jsx
+++ src/pages/preview.jsx
@@ -8,7 +8,7 @@ const Preview = (props) => {
         <p>id: {props.id}をゲット</p>
         <p>title: {props.title}をゲット</p>
         <p>body: {props.body}をゲット</p>
-        <img src={props.url}/>
+        <img src={props.img}/>
       </div>
     </main>
   );
@@ -19,7 +19,7 @@ export default Preview;
 export async function getServerSideProps(context) {
   const id = context.query.slug;
   const draftKey = context.query.draftKey;
-  const {data} = await axios.get(`https://myblogadachi.microcms.io/api/v1/blog/${id}?draftKey=${draftKey}`, {headers: {"X-MICROCMS-API-KEY": "hnhz0PP2alUXE7wIGrNf5GsNMbqJmEOZ1lno"}})
+  const {data} = await axios.get(`https://myblogadachi.microcms.io/api/v1/blog/${id}?draftKey=${draftKey}`, {headers: {"X-MICROCMS-API-KEY": process.env.API_KEY}})
 
   return {
     props: {
