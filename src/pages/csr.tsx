5d3906f66c7207af1a1a095ea6c7fa72bc215f9f
adachi-shu <backnumber0620@icloud.com>
2023-01-30 15:01:12 +0900

    edit csr

--- src/pages/csr.tsx
+++ src/pages/csr.tsx
@@ -5,18 +5,24 @@ import { useRouter } from 'next/router';
 
 const fetcher = async (context: any) => {
   const {slug, draftKey} = context.query;
-  // const data = await client.get({
-  //   endpoint: "blog",
-  //   contentId: slug,
-  //   queries: { draftKey: draftKey }
-  // });
-  const {data} = await axios.get(`https://myblogadachi.microcms.io/api/v1/blog/${slug}?draftKey=${draftKey}`, {headers: {"X-MICROCMS-API-KEY": process.env.API_KEY}})
+  const data = await client.get({
+    endpoint: "blog",
+    contentId: slug,
+    queries: { draftKey: draftKey }
+  });
+  // const res = await fetch(`https://myblogadachi.microcms.io/api/v1/blog/${slug}?draftKey=${draftKey}`, {headers: {"X-MICROCMS-API-KEY": process.env.API_KEY}})
+  // const data = res.json();
   return data;
 }
 
 export default function CSR() {
   const router = useRouter();
-  const {data, error} = useSWR(router, fetcher);
+  const {data, error} = useSWR(
+    router,
+    fetcher,
+    {
+      refreshInterval: 20000,
+    });
 
   if(error) return "An error has occured";
   if(!data) return "Loading ...";
